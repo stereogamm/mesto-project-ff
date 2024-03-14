fc23c4be80c7497359a53e7906afb6860c8160d2
Marina Sysoliatina <stereogam@yandex.ru>
2024-03-14 09:16:36 +0100

    functionality_without_delete_button

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const page = document.querySelector('.page');
+const content = page.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content; 
+
+
+function createCards(card, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteCardButton = cardTemplate.querySelector('.places__item .card__delete-button');
+    const likeCardButton = cardTemplate.querySelector('.card__description .card__like-button');
+
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    
+    deleteCardButton.addEventListener('click', () => {deleteCard(cardElement);}); 
+    placesList.append(cardElement); 
+}
+
+for (let i = 0; i < initialCards.length ; i++) {
+    createCards(initialCards[i]);
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
